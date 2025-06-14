000840d922155e53250c60a1a6ab5bfa857cd8c2
harden <rocketfans36@gmail.com>
2025-06-14 15:55:52 +0800

    feat: cookieToInitialState update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,26 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
 import { headers } from "next/headers";
 import { cookieToInitialState } from "wagmi";
 import { getConfig } from "@/wagmi";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
 export const metadata: Metadata = {
   title: "web3 assistant",
   description: "help your to manage your web3 assets",
 };
 
-const initialState = cookieToInitialState(
-  getConfig(),
-  (await headers()).get('cookie'),
-)
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const initialState = cookieToInitialState(
+    getConfig(),
+    (await headers()).get('cookie'),
+  )
+  
   return (
     <html lang="en">
       <body
